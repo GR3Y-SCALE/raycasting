91e747d5d3a64a7ef64de0282d945e2f0e243f6d
Maurice Blake <62605251+GR3Y-SCALE@users.noreply.github.com>
2021-06-13 18:17:01 +0930

    Add files via upload

--- js_raycast/raycast.js
+++ js_raycast/raycast.js
@@ -1,13 +1,15 @@
-const TILE_SIZE = 32;
+const TILE_SIZE = 80;
 const MAP_NUM_ROWS = 11;
 const MAP_NUM_COLS = 15;
 
 const WINDOW_WIDTH = MAP_NUM_COLS * TILE_SIZE;
 const WINDOW_HEIGHT = MAP_NUM_ROWS * TILE_SIZE;
 
-const FOV_ANGLE = 60 * (Math.PI / 180); //"60" degrees in radians
+const MINIMAP_SCALE = 0.2; // scaling factor of the minimap
 
-const WALL_STRIPE_WIDTH = 30;
+const FOV_ANGLE = 60 * (Math.PI / 180); // "60" degrees in radians
+
+const WALL_STRIPE_WIDTH = 1;
 const NUM_RAYS = WINDOW_WIDTH / WALL_STRIPE_WIDTH;
 
 
@@ -46,7 +48,12 @@ class Map {
                 var tileColor = this.grid[i][j] == 1 ? "#222" : "#fff";
                 stroke("#222");
                 fill(tileColor);
-                rect(tileX, tileY, TILE_SIZE, TILE_SIZE);
+                rect(
+			MINIMAP_SCALE * tileX,
+			MINIMAP_SCALE * tileY,
+			MINIMAP_SCALE * TILE_SIZE,
+			MINIMAP_SCALE * TILE_SIZE
+		);
             }
         }
     }
@@ -61,8 +68,8 @@ class Player {
 		this.turnDirection = 0; // -1 if left, +1 if right
 		this.walkDirection = 0; // -1 if back, +1 if front
 		this.rotationAngle = Math.PI / 2; //random angle
-		this.moveSpeed = 2.0; //change if too fast/slow
-		this.rotationSpeed = 2 * (Math.PI / 180); //rotation angle is "2" in radians
+		this.moveSpeed = 2.5; //change if too fast/slow
+		this.rotationSpeed = 1.8 * (Math.PI / 180); //rotation angle is "2" in radians
 	}
 	update() {
 		this.rotationAngle += this.turnDirection * this.rotationSpeed; //plus the rotation angle by the turnDirection and speed
@@ -79,19 +86,24 @@ class Player {
 		}
 		//console.log(floor(this.x / TILE_SIZE) + ' ' + floor(this.y / TILE_SIZE)); //shows position on grid (x,y)
 		
-		console.log('angle ' + this.rotationAngle + ' X and Y ' + this.x + ' ' + this.y);// log the rotationAngle, X and Y coordinates
+		// console.log('angle ' + this.rotationAngle + ' X and Y ' + this.x + ' ' + this.y);// log the rotationAngle, X and Y coordinates
 
 	}
 	render() {
 		noStroke();
 		fill("red");
-		circle(this.x, this.y, this.radius);
-		stroke("red");
+		circle(
+			MINIMAP_SCALE * this.x,
+			MINIMAP_SCALE * this.y,
+			MINIMAP_SCALE * this.radius
+		);
+
+		stroke("blue");
 		line(
-			this.x, //x coordinate of the start of the line
-			this.y, //y coordinate of the start of the line
-			this.x + Math.cos(this.rotationAngle) * 30, //x coodinate of the end of the line, 30 pixels from x-start
-			this.y + Math.sin(this.rotationAngle) * 30 //y coordingate of the end of the line, 30 pixels from y-start
+			MINIMAP_SCALE * this.x, // x coordinate of the start of the line
+			MINIMAP_SCALE * this.y, // y coordinate of the start of the line
+			MINIMAP_SCALE * (this.x + Math.cos(this.rotationAngle) * 30), // x coodinate of the end of the line, 30 pixels from x-start
+			MINIMAP_SCALE * (this.y + Math.sin(this.rotationAngle) * 30) // y coordingate of the end of the line, 30 pixels from y-start
 		);
 	5}
 }
@@ -103,21 +115,149 @@ class Ray {
 		this.wallHitX = 0;
 		this.wallHitY = 0;
 		this.distance = 0;
+		this.wasHitVertical = false;
+
+		this.isRayFacingDown = this.rayAngle > 0 && this.rayAngle < Math.PI; // if the ray is greater then 0 and below pi, pi is 270 degrees, therefore the ray is facing down
+		this.isRayFacingUp = !this.isRayFacingDown; // opposite of rayFacingDown
+
+		this.isRayFacingRight = this.rayAngle < 0.5 * Math.PI || this.rayAngle > 1.5 * Math.PI; // more then 270 deg (1.5 * PI) but below 90 (PI/2) means ray is facing right
+		this.isRayFacingLeft = !this.isRayFacingRight; // opposite of rayFacingRight
 	}
 	cast(columnid) {
+		var xintercept, yintercept;
+		var xstep, ystep;
 		///////////////////////////////////////////
 		// HORIZONTAL RAY-GRID INTERSECTION CODE //
 		///////////////////////////////////////////
-		//TODO:...
+		
+		var foundHorzWallHit = false;
+		var horzWallHitX = 0;
+		var horzWallHitY = 0;
+		
+		// console.log("isRayFacingDown?", this.isRayFacingDown);
+		// console.log("isRayFacingRight?", this.isRayFacingRight);
+
+		// find the y-coordinate of the closest horizontal grid intersection
+		yintercept = Math.floor(player.y / TILE_SIZE) * TILE_SIZE;
+		yintercept += this.isRayFacingDown ? TILE_SIZE : 0; // IF ray is facing down then add tile_size ELSE do nothing
+
+		// find the x-coordinate of the closest horizontal grid intersection
+		xintercept = player.x + (yintercept - player.y) / Math.tan(this.rayAngle);
+
+		// calculate the increment xStep and yStep (DeltaX and DeltaY)
+		ystep = TILE_SIZE;
+		ystep *= this.isRayFacingUp ? -1 : 1; // if the ray is facing up, invert ystep
+
+		xstep = TILE_SIZE / Math.tan(this.rayAngle);
+		xstep *= (this.isRayFacingLeft && xstep > 0) ? -1 : 1;
+		xstep *= (this.isRayFacingRight && xstep < 0) ? -1 : 1;
+
+		var nextHorzTouchX = xintercept;
+		var nextHorzTouchY = yintercept;
+
+		// if (this.isRayFacingUp) {
+		//	nextHorzTouchY--;
+		// }
+
+		// increment xstep and ystep until a wall is found
+		while (nextHorzTouchX >= 0 && nextHorzTouchX <= WINDOW_WIDTH && nextHorzTouchY >= 0 && nextHorzTouchY <= WINDOW_HEIGHT) {
+			if (grid.hasWallAt(nextHorzTouchX, nextHorzTouchY - (this.isRayFacingUp ? 1 : 0))) {
+				// a wall is hit
+				foundHorzWallHit = true;
+				horzWallHitX = nextHorzTouchX;
+				horzWallHitY = nextHorzTouchY;
+
+				// testing
+				// stroke("red");
+				// line(player.x, player.y, horzWallHitX, horzWallHitY); // line from player to the wall hit
+				break; // TODO: change the statement so it isnt a break
+
+			} else {
+				nextHorzTouchX += xstep; // move to the next intercept (x,...) 
+				nextHorzTouchY += ystep; // move to the next intercept (...,y)
+			}
+		}
+
+		/////////////////////////////////////////
+		// VERTICAL RAY-GRID INTERSECTION CODE //
+		/////////////////////////////////////////
+		
+		var foundVertWallHit = false;
+		var vertWallHitX = 0;
+		var vertWallHitY = 0;
+		
+		// console.log("isRayFacingDown?", this.isRayFacingDown);
+		// console.log("isRayFacingRight?", this.isRayFacingRight);
+
+		// find the x-coordinate of the closest vertical grid intersection
+		xintercept = Math.floor(player.x / TILE_SIZE) * TILE_SIZE;
+		xintercept += this.isRayFacingRight ? TILE_SIZE : 0; // IF ray is facing Right then add tile_size ELSE do nothing
+
+		// find the y-coordinate of the closest vetical grid intersection
+		yintercept = player.y + (xintercept - player.x) * Math.tan(this.rayAngle);
+
+		// calculate the increment xStep and yStep (DeltaX and DeltaY)
+		xstep = TILE_SIZE;
+		xstep *= this.isRayFacingLeft ? -1 : 1; // if the ray is facing up, invert ystep
+
+		ystep = TILE_SIZE * Math.tan(this.rayAngle);
+		ystep *= (this.isRayFacingUp && ystep > 0) ? -1 : 1;
+		ystep *= (this.isRayFacingDown && ystep < 0) ? -1 : 1;
+
+		var nextVertTouchX = xintercept;
+		var nextVertTouchY = yintercept;
+
+		// if (this.isRayFacingLeft) {
+		//	nextVertTouchX--;
+		// }
+
+		// increment xstep and ystep until a wall is found
+		while (nextVertTouchX >= 0 && nextVertTouchX <= WINDOW_WIDTH && nextVertTouchY >= 0 && nextVertTouchY <= WINDOW_HEIGHT) {
+			if (grid.hasWallAt(nextVertTouchX - (this.isRayFacingLeft ? 1 : 0), nextVertTouchY)) {
+				// a wall is hit
+				foundVertWallHit = true;
+				vertWallHitX = nextVertTouchX;
+				vertWallHitY = nextVertTouchY;
+
+				// testing
+				// stroke("red");
+				// line(player.x, player.y, vertWallHitX, vertWallHitY); // line from player to the wall hit
+				break; // TODO: change the statement so it isnt a break
+
+			} else {
+				nextVertTouchX += xstep; // move to the next intercept (x,...) 
+				nextVertTouchY += ystep; // move to the next intercept (...,y)
+			}
+		}
+
+		// Calculate both horizontal and vertical distances and choose the smallest value
+		var horzHitDistance = (foundHorzWallHit)  // if true then it will find the distance, otherwise it is assumed that vertHit is smaller
+			? distanceBetweenPoints(player.x, player.y, horzWallHitX, horzWallHitY) 
+			: Number.MAX_VALUE;
+		var vertHitDistance = (foundVertWallHit) // if true then it will find the distance, otherwise it is assumed that horzHit is smaller
+			? distanceBetweenPoints(player.x, player.y, vertWallHitX, vertWallHitY)
+			: Number.MAX_VALUE;
+
+		// only store the smallest of the distances
+		this.wallHitX = (horzHitDistance < vertHitDistance) 
+			? horzWallHitX 
+			: vertWallHitX;
+		this.wallHitY = (horzHitDistance < vertHitDistance) 
+			? horzWallHitY 
+			: vertWallHitY;
+		this.distance = (horzHitDistance < vertHitDistance) 
+			? horzHitDistance 
+			: vertHitDistance;
+		this.wasHitVertical = (vertHitDistance < horzHitDistance); // wasHitVertical is true if vertDistance is smaller
 	}
 
 	render() {
 		stroke("rgba(255, 0, 0, 0.3)");
 		line(
-			player.x,
-			player.y,
-			player.x + Math.cos(this.rayAngle) * 30,
-			player.y + Math.sin(this.rayAngle) * 30
+			MINIMAP_SCALE * player.x,
+			MINIMAP_SCALE * player.y,
+			MINIMAP_SCALE * this.wallHitX,
+			MINIMAP_SCALE * this.wallHitY
 		);
 	}
 }	
@@ -162,9 +302,7 @@ function castAllRays() {
 	rays = [];
 
 	// loop all columns casting the rays
-	//for (var i = 0; i < NUM_RAYS; i++) {
-	for (var i = 0; i < 1; i++) {
-
+	for (var i = 0; i < NUM_RAYS; i++) {
 		var ray = new Ray(rayAngle);
 		ray.cast(columnId);
 		rays.push(ray);
@@ -183,6 +321,10 @@ function normaliseAngle(angle) {
 	return angle;
 }
 
+function distanceBetweenPoints(x1, y1, x2, y2) {
+	return Math.sqrt((x2 -x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)); // return the length between the two points, hypotenuse. X & Y being A & B
+}
+
 function setup() {
 	createCanvas(WINDOW_WIDTH, WINDOW_HEIGHT);
 
@@ -190,7 +332,7 @@ function setup() {
 
 function update() {
 	player.update();
-	castAllRays();
+	castAllRays(); 
 }
 
 function draw() {
